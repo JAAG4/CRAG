924b054722716a4bad57d1af7fa851fae226b2ee
JAAG <45705167+JAAG4@users.noreply.github.com>
2026-05-06 20:28:58 -0400

    Create directory for saving evaluator models
    Add model saving directory creation for evaluator training

--- run_evaluator_training.sh
+++ run_evaluator_training.sh
@@ -3,6 +3,9 @@ batch_size=BATCH_SIZE
 num_epochs=N_EPOCH
 seed=SEED
 
+save_path="../models/my_evaluator"
+mkdir -p ../models/my_evaluator
+
 python train_evaluator.py \
 --train_file ../data/popqa/train_popqa.txt \
 --save_path YOUR_EVALUATOR_PATH --batch_size $batch_size \
