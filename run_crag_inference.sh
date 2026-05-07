d1f31bd45486c0d4704dfa690c8d4ce10d112a9b
JAAG <45705167+JAAG4@users.noreply.github.com>
2026-05-06 20:23:13 -0400

    Change dataset variable from 'popqa' to 'pubqa'

--- run_crag_inference.sh
+++ run_crag_inference.sh
@@ -2,7 +2,7 @@
 
 cd scripts
 
-dataset=popqa
+dataset=pubqa
 python CRAG_Inference.py \
 --generator_path YOUR_GENERATOR_PATH \
 --evaluator_path YOUR_EVALUATOR_PATH \
@@ -13,14 +13,3 @@ python CRAG_Inference.py \
 --combined_knowledge_path ../data/$dataset/ref/ambiguous \
 --task $dataset --method crag --device cuda:0 \
 --ndocs 10 --batch_size 8 --upper_threshold 0.592 --lower_threshold 0.995
-
-# python CRAG_Inference.py \
-# --generator_path YOUR_GENERATOR_PATH \
-# --evaluator_path YOUR_EVALUATOR_PATH \
-# --input_file ../data/pubqa/test_pubqa.txt \
-# --output_file ../data/pubqa/output/YOUR_OUTPUT_FILE \
-# --internal_knowledge_path ../data/pubqa/ref/correct \
-# --external_knowledge_path ../data/pubqa/ref/incorrect \
-# --combined_knowledge_path ../data/pubqa/ref/ambiguous \
-# --task pubqa --method crag --device cuda:0 \
-# --ndocs 10 --batch_size 8 --upper_threshold 0.5 --lower_threshold 0.915
