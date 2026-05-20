e6986e5e74232480d7c3cf57a0cf146c31772e5b
JAAG4 <45705167+JAAG4@users.noreply.github.com>
2026-05-20 11:25:48 -0400

    Refactor tracing spans for dataset processing and improve postprocess logging

--- scripts/data_process.py
+++ scripts/data_process.py
@@ -36,11 +36,13 @@ def main():
             "dataset": args.dataset,
             "input_file": input_file,
             "count": len(input_data),
-            "pre": pre,
+            "--postprocess": pre,
         }
     ):
         if dataset == "bio":
-            with tracer.start_as_current_span("bio_processing",openinference_span_kind="chain") as span:
+            with tracer.start_as_current_span(
+                "bio_processing", openinference_span_kind="chain"
+            ) as span:
                 questions = []
                 passages = []
                 contents = []
@@ -73,7 +75,9 @@ def main():
                 )
 
         elif dataset == "arc_challenge":
-            with tracer.start_as_current_span("arc_processing",openinference_span_kind="chain") as span:
+            with tracer.start_as_current_span(
+                "arc_processing", openinference_span_kind="chain"
+            ) as span:
                 questions = []
                 passages = []
                 choice_contents = []
@@ -121,7 +125,9 @@ def main():
                 )
 
         elif dataset == "pubqa":
-            with tracer.start_as_current_span("pubqa_processing",openinference_span_kind="chain") as span:
+            with tracer.start_as_current_span(
+                "pubqa_processing", openinference_span_kind="chain"
+            ) as span:
                 questions = []
                 passages = []
                 contents = []
@@ -152,7 +158,9 @@ def main():
                 )
 
         elif dataset == "popqa":
-            with tracer.start_as_current_span("popqa_processing",openinference_span_kind="chain") as span:
+            with tracer.start_as_current_span(
+                "popqa_processing", openinference_span_kind="chain"
+            ) as span:
                 questions = []
                 passages = []
                 contents = []
@@ -188,7 +196,10 @@ def main():
                 )
 
         if pre:
-            with tracer.start_as_current_span("postprocess",openinference_span_kind="chain") as span:
+            print("args.postprocess\t", args.postprocess)
+            with tracer.start_as_current_span(
+                "postprocess", openinference_span_kind="chain"
+            ) as span:
                 with open("../data/{}/ref/correct".format(dataset), "r") as f:
                     contexts = [l.strip()[1:] for l in f.readlines()]
                 with open("../data/{}/ref/incorrect".format(dataset), "r") as f:
