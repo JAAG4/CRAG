afaad89b0681c1c43e04b8f5639ed92c52bffdaa
JAAG4 <45705167+JAAG4@users.noreply.github.com>
2026-05-27 14:58:01 -0400

    Fixed wrong output

--- scripts/combined_knowledge_preparation.py
+++ scripts/combined_knowledge_preparation.py
@@ -1,21 +1,25 @@
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--correct_path', type=str)
-    parser.add_argument('--incorrect_path', type=str)
-    parser.add_argument('--ambiguous_path', type=str)
+    parser.add_argument("--correct_path", type=str)
+    parser.add_argument("--incorrect_path", type=str)
+    parser.add_argument("--ambiguous_path", type=str)
     args = parser.parse_args()
 
-    with open(args.correct_path, 'r') as f:
+    with open(args.correct_path, "r") as f:
         correct_lines = [q.strip()[1:] for q in f.readlines()]
-    with open(args.incorrect_path, 'r') as f:
+    with open(args.incorrect_path, "r") as f:
         incorrect_lines = [q.strip()[1:] for q in f.readlines()]
     ambiguous_lines = []
-    for correct, incorrect in enumerate(zip(correct_lines, incorrect_lines)):
-        ambiguous_lines.append("Knowledge1: " + str(correct) + " [sep] Knowledge2: " + str(incorrect))
-    with open(args.ambiguous_path, 'w') as f:
-        f.write('\n'.join(ambiguous_lines))
+    for correct, incorrect in zip(correct_lines, incorrect_lines):
+        ambiguous_lines.append(
+            "Knowledge1: " + str(correct) + " [sep] Knowledge2: " + str(incorrect)
+        )
+    with open(args.ambiguous_path, "w") as f:
+        f.write("\n".join(ambiguous_lines))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
